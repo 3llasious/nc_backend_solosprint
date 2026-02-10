f0344f41956ed69317a36418899efb59835bd474
3llasious <quest4ella22@gmail.com>
2026-02-10 10:50:45 +0000

    added a seed production db script

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,6 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool(config);
-
 if (!process.env.PGDATABASE) {
   throw new Error("No PGDATABASE configured");
 } else {
@@ -14,9 +12,11 @@ if (!process.env.PGDATABASE) {
 
 const config = {};
 
+const db = new Pool(config);
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-}
+} // configiration check, passed into pool connection, max connections set to two
 
 module.exports = db;
