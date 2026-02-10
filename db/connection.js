858bcedbd73b6302372bae76c4fecfffafddc533
3llasious <quest4ella22@gmail.com>
2026-02-10 10:55:17 +0000

    fixed mistakes in connection.js and successfully connected to production db

--- db/connection.js
+++ db/connection.js
@@ -4,19 +4,19 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
-
 const config = {};
-
-const db = new Pool(config);
-
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 } // configiration check, passed into pool connection, max connections set to two
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE configured");
+} else {
+  console.log(
+    `Connected to ${process.env.PGDATABASE || "production database"}`,
+  );
+}
+const db = new Pool(config);
 
 module.exports = db;
