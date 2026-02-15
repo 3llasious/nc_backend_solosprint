6eadd7d20a5eb17163bd7261066cbf3b0e29702d
3llasious <quest4ella22@gmail.com>
2026-02-15 21:19:17 +0000

    Fix port variable and console.log syntax

--- listen.js
+++ listen.js
@@ -7,5 +7,5 @@ const app = require("./app.js");
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${PORT}...`);
+  console.log(`Server is listening on port ${port}...`);
 });
