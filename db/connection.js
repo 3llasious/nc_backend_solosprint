b60ac3fa976282de7fbf4575bf32675cd9f2470a
3llasious <quest4ella22@gmail.com>
2026-02-15 22:16:18 +0000

    hardcode test

--- db/connection.js
+++ db/connection.js
@@ -1,32 +1,10 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || "development";
-
-if (ENV !== "production") {
-  require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-}
-
-const config = {};
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-  config.ssl = { rejectUnauthorized: false }; // CRITICAL FOR SUPABASE!
-  // Force these to be undefined so they don't override
-  delete process.env.PGHOST;
-  delete process.env.PGUSER;
-  delete process.env.PGPASSWORD;
-  delete process.env.PGDATABASE;
-  delete process.env.PGPORT;
-} // configiration check, passed into pool connection, max connections set to two
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("No PGDATABASE configured");
-} else {
-  console.log(
-    `Connected to ${process.env.PGDATABASE || "production database"}`,
-  );
-}
-
-const db = new Pool(config);
+const db = new Pool({
+  connectionString:
+    "postgresql://postgres:Oizaitop01!!@db.htvhqjnxjyjexrmijhwh.supabase.co:5432/postgres",
+  ssl: { rejectUnauthorized: false },
+  max: 2,
+});
 
 module.exports = db;
