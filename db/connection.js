7102923ad7fae112b188f5217316490dc264600c
3llasious <quest4ella22@gmail.com>
2026-02-24 16:53:35 +0000

    fixed seeding issue with correct prod data

--- db/connection.js
+++ db/connection.js
@@ -2,22 +2,15 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
-if (ENV !== "production") {
-  require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-}
+console.log(ENV, "<< ENV");
+
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-  config.ssl = { rejectUnauthorized: false }; // CRITICAL FOR SUPABASE!
-  // Force these to be undefined so they don't override
-  delete process.env.PGHOST;
-  delete process.env.PGUSER;
-  delete process.env.PGPASSWORD;
-  delete process.env.PGDATABASE;
-  delete process.env.PGPORT;
-} // configiration check, passed into pool connection, max connections set to two
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE configured");
